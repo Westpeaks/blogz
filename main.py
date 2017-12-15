d697db6d00cf8a361be361a0c299bd62fa273c77
Wesley Peak <wesley.peak@gmail.com>
2017-12-14 19:28:11 -0600

    working version with db

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 from flask import Flask, request, redirect, render_template
-import cgi
-import os
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Build-a-Blog2:12Kamelz@localhost:8889/Build-a-Blog2'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
 
-headings = []
-entries = []
+class Blog(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(1200))
 
-@app.route('/', methods=['POST', 'GET'])
-def blog_posts():
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+@app.route('/new_post', methods=['POST', 'GET'])
+def new_post():
     
     if request.method == 'POST':
-        heading = request.form['heading']
-        entry = request.form['entry']
-        headings.append(heading)
-        entries.append(entry)
+        entry_name = request.form['title']
+        entry_body = request.form['body']
+        new_entry = Blog(entry_name, entry_body)
+        db.session.add(new_entry)
+        db.session.commit()
+        return redirect("/single_post?id="+ str(new_entry.id))
+    
+    return render_template('new_post.html')
 
-    return render_template('blog.html', title="BuildABlog", headings=headings, entries=entries)
+@app.route('/single_post', methods =['POST','GET'])
+def blog():
+    if request.args:
+        blog_id = request.args.get("id")
+        blog_entry = Blog.query.get(blog_id)
+    return render_template('single_post.html', blog=blog_entry)
+        
+@app.route('/', methods=['POST', 'GET'])
+def index():
+        
+    entries = Blog.query.all()
 
+    return render_template('blog.html', entries=entries)
 
-app.run()
+if __name__ == '__main__':
+    app.run()
